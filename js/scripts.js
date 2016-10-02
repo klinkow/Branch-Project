6c2a2fc71b0b1824ac932c9a2da5508f7ff834df
Billybob <blank>
2016-10-01 18:50:03 -0700

    create a functioning website that will take user input and provide a recommendation for a restaurant in SE Portland using branching.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,40 @@
 $(document).ready(function() {
 
+  $("button").click(function(event) {
 
+    var name = $("#name").val();
+    $(".hello").show();
+    $(".name").text(name);
+
+    var cuisine = $("#cuisine").val();
+    var cost = $("#cost").val();
+
+    if (cuisine==="Asian" && cost==="MM") {
+      $(".AsMM").show();
+    } else if (cuisine === "European" && cost==="MM") {
+      $(".EuMM").show();
+    } else if (cuisine === "American" && cost==="MM") {
+      $(".AmMM").show();
+    } else if (cuisine === "African" && cost==="MM") {
+      $(".AfMM").show();
+    } else if (cuisine === "Modern-Fusion" && cost==="MM") {
+      $(".MoMM").show();
+    } else if (cuisine === "Latin American" && cost==="MM") {
+      $(".LaMM").show();
+    } else if (cuisine==="Asian" && cost==="MMM") {
+      $(".AsMMM").show();
+    } else if (cuisine === "European" && cost==="MMM") {
+      $(".EuMMM").show();
+    } else if (cuisine === "American" && cost==="MMM") {
+      $(".AmMMM").show();
+    } else if (cuisine === "African" && cost==="MMM") {
+      $(".AfMMM").show();
+    } else if (cuisine === "Modern-Fusion" && cost==="MMM") {
+      $(".MoMMM").show();
+    } else if (cuisine === "Latin American" && cost==="MMM") {
+      $(".LaMMM").show();
+    }
+
+
+  })
 })
