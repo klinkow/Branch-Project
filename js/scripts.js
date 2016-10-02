647f658a79c68d679336544b76d44d530086a6f0
Billybob <blank>
2016-10-02 09:50:17 -0700

    remove old entries when the submit button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -11,28 +11,125 @@ $(document).ready(function() {
 
     if (cuisine==="Asian" && cost==="MM") {
       $(".AsMM").show();
+      $(".EuMM").hide();
+      $(".AmMM").hide();
+      $(".AfMM").hide();
+      $(".MoMM").hide();
+      $(".LaMM").hide();
+      $(".AsMMM").hide();
+      $(".EuMMM").hide();
+      $(".AmMMM").hide();
+      $(".AfMMM").hide();
+      $(".MoMMM").hide();
+      $(".LaMMM").hide();
     } else if (cuisine === "European" && cost==="MM") {
       $(".EuMM").show();
+      $(".AmMM").hide();
+      $(".AfMM").hide();
+      $(".MoMM").hide();
+      $(".LaMM").hide();
+      $(".AsMMM").hide();
+      $(".EuMMM").hide();
+      $(".AmMMM").hide();
+      $(".AfMMM").hide();
+      $(".MoMMM").hide();
+      $(".LaMMM").hide();
     } else if (cuisine === "American" && cost==="MM") {
       $(".AmMM").show();
-    } else if (cuisine === "African" && cost==="MM") {
-      $(".AfMM").show();
+      $(".EuMM").hide();
+      $(".AfMM").hide();
+      $(".MoMM").hide();
+      $(".LaMM").hide();
+      $(".AsMMM").hide();
+      $(".EuMMM").hide();
+      $(".AmMMM").hide();
+      $(".AfMMM").hide();
+      $(".MoMMM").hide();
+      $(".LaMMM").hide();
     } else if (cuisine === "Modern-Fusion" && cost==="MM") {
       $(".MoMM").show();
+      $(".EuMM").hide();
+      $(".AmMM").hide();
+      $(".AfMM").hide();
+      $(".LaMM").hide();
+      $(".AsMMM").hide();
+      $(".EuMMM").hide();
+      $(".AmMMM").hide();
+      $(".AfMMM").hide();
+      $(".MoMMM").hide();
+      $(".LaMMM").hide();
     } else if (cuisine === "Latin American" && cost==="MM") {
       $(".LaMM").show();
+      $(".EuMM").hide();
+      $(".AmMM").hide();
+      $(".AfMM").hide();
+      $(".MoMM").hide();
+      $(".AsMMM").hide();
+      $(".EuMMM").hide();
+      $(".AmMMM").hide();
+      $(".AfMMM").hide();
+      $(".MoMMM").hide();
+      $(".LaMMM").hide();
     } else if (cuisine==="Asian" && cost==="MMM") {
       $(".AsMMM").show();
+      $(".EuMM").hide();
+      $(".AmMM").hide();
+      $(".AfMM").hide();
+      $(".MoMM").hide();
+      $(".LaMM").hide();
+      $(".EuMMM").hide();
+      $(".AmMMM").hide();
+      $(".AfMMM").hide();
+      $(".MoMMM").hide();
+      $(".LaMMM").hide();
     } else if (cuisine === "European" && cost==="MMM") {
       $(".EuMMM").show();
+      $(".EuMM").hide();
+      $(".AmMM").hide();
+      $(".AfMM").hide();
+      $(".MoMM").hide();
+      $(".LaMM").hide();
+      $(".AsMMM").hide();
+      $(".AmMMM").hide();
+      $(".AfMMM").hide();
+      $(".MoMMM").hide();
+      $(".LaMMM").hide();
     } else if (cuisine === "American" && cost==="MMM") {
       $(".AmMMM").show();
-    } else if (cuisine === "African" && cost==="MMM") {
-      $(".AfMMM").show();
+      $(".EuMM").hide();
+      $(".AmMM").hide();
+      $(".AfMM").hide();
+      $(".MoMM").hide();
+      $(".LaMM").hide();
+      $(".AsMMM").hide();
+      $(".EuMMM").hide();
+      $(".AfMMM").hide();
+      $(".MoMMM").hide();
+      $(".LaMMM").hide();
     } else if (cuisine === "Modern-Fusion" && cost==="MMM") {
       $(".MoMMM").show();
+      $(".EuMM").hide();
+      $(".AmMM").hide();
+      $(".AfMM").hide();
+      $(".MoMM").hide();
+      $(".LaMM").hide();
+      $(".AsMMM").hide();
+      $(".EuMMM").hide();
+      $(".AmMMM").hide();
+      $(".AfMMM").hide();
+      $(".LaMMM").hide();
     } else if (cuisine === "Latin American" && cost==="MMM") {
       $(".LaMMM").show();
+      $(".EuMM").hide();
+      $(".AmMM").hide();
+      $(".AfMM").hide();
+      $(".MoMM").hide();
+      $(".LaMM").hide();
+      $(".AsMMM").hide();
+      $(".EuMMM").hide();
+      $(".AmMMM").hide();
+      $(".AfMMM").hide();
+      $(".MoMMM").hide();
     }
 
 
